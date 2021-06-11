6deeade568517dee933ae22b104e086a995aa58b
9boogie <jzero.youngs89@gmail.com>
2021-06-11 16:07:24 -0700

    init project

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Homepage from "./Component/Homepage";
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
