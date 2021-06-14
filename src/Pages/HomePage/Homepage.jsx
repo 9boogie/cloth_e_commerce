a43c9bd969f47383bbda80bba9564635294ce6f9
9boogie <jzero.youngs89@gmail.com>
2021-06-14 00:44:34 -0700

    break homepage into components

--- src/Pages/HomePage/Homepage.jsx
+++ src/Pages/HomePage/Homepage.jsx
@@ -1,41 +1,11 @@
 import React from "react";
+import Directory from "../../Components/directory/directory.component";
 import "./Homepage.scss";
 
 const Homepage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Jacket</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Sneakers</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Womens</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">Mens</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 };
